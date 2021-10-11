5a1adc023c4cf20e1fee4ecfd3e25ba4f47e23c1
Katerina <katerina.yakybych@gmail.com>
2021-10-12 00:26:05 +0300

    revisions

--- js/script.js
+++ js/script.js
@@ -5,7 +5,10 @@ const menu = document.querySelector(".menu");
 let cost = 90;
 let totalPrice = 0;
 schemeSvg.addEventListener("click", (event) => {
-  if (!event.target.classList.contains("booked")) {
+  if (
+    !event.target.classList.contains("booked") &&
+    !event.target.classList.contains("light")
+  ) {
     event.target.classList.toggle("active");
     let totalSeats = schemeSvg.querySelectorAll(".active").length;
     totalPrice = totalSeats * cost;
